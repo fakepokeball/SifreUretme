63f5795813364e9f36a40e770ad4236e6bd473df
cowan <cowan@DESKTOP-E1DT78U>
2022-07-25 16:43:28 +0300

    Add project files.

---
+++ SifreUretme/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace SifreUretme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxParametreler = new System.Windows.Forms.GroupBox();
+            this.cbxKucukHarf = new System.Windows.Forms.CheckBox();
+            this.cbxBuyukHarf = new System.Windows.Forms.CheckBox();
+            this.cbxRakam = new System.Windows.Forms.CheckBox();
+            this.cbxSembol = new System.Windows.Forms.CheckBox();
+            this.lblSifreAdedi = new System.Windows.Forms.Label();
+            this.lblSifreUzunlugu = new System.Windows.Forms.Label();
+            this.nudSifreAdedi = new System.Windows.Forms.NumericUpDown();
+            this.nudSifreUzunlugu = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.gbxParametreler.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSifreAdedi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSifreUzunlugu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbxParametreler
+            // 
+            this.gbxParametreler.Controls.Add(this.btnUret);
+            this.gbxParametreler.Controls.Add(this.nudSifreUzunlugu);
+            this.gbxParametreler.Controls.Add(this.nudSifreAdedi);
+            this.gbxParametreler.Controls.Add(this.lblSifreUzunlugu);
+            this.gbxParametreler.Controls.Add(this.lblSifreAdedi);
+            this.gbxParametreler.Controls.Add(this.cbxSembol);
+            this.gbxParametreler.Controls.Add(this.cbxRakam);
+            this.gbxParametreler.Controls.Add(this.cbxBuyukHarf);
+            this.gbxParametreler.Controls.Add(this.cbxKucukHarf);
+            this.gbxParametreler.Location = new System.Drawing.Point(12, 12);
+            this.gbxParametreler.Name = "gbxParametreler";
+            this.gbxParametreler.Size = new System.Drawing.Size(219, 231);
+            this.gbxParametreler.TabIndex = 0;
+            this.gbxParametreler.TabStop = false;
+            this.gbxParametreler.Text = "Parametreler";
+            // 
+            // cbxKucukHarf
+            // 
+            this.cbxKucukHarf.AutoSize = true;
+            this.cbxKucukHarf.Location = new System.Drawing.Point(6, 19);
+            this.cbxKucukHarf.Name = "cbxKucukHarf";
+            this.cbxKucukHarf.Size = new System.Drawing.Size(80, 17);
+            this.cbxKucukHarf.TabIndex = 0;
+            this.cbxKucukHarf.Text = "Küçük Harf";
+            this.cbxKucukHarf.UseVisualStyleBackColor = true;
+            // 
+            // cbxBuyukHarf
+            // 
+            this.cbxBuyukHarf.AutoSize = true;
+            this.cbxBuyukHarf.Location = new System.Drawing.Point(6, 42);
+            this.cbxBuyukHarf.Name = "cbxBuyukHarf";
+            this.cbxBuyukHarf.Size = new System.Drawing.Size(79, 17);
+            this.cbxBuyukHarf.TabIndex = 1;
+            this.cbxBuyukHarf.Text = "Büyük Harf";
+            this.cbxBuyukHarf.UseVisualStyleBackColor = true;
+            // 
+            // cbxRakam
+            // 
+            this.cbxRakam.AutoSize = true;
+            this.cbxRakam.Location = new System.Drawing.Point(6, 65);
+            this.cbxRakam.Name = "cbxRakam";
+            this.cbxRakam.Size = new System.Drawing.Size(60, 17);
+            this.cbxRakam.TabIndex = 2;
+            this.cbxRakam.Text = "Rakam";
+            this.cbxRakam.UseVisualStyleBackColor = true;
+            // 
+            // cbxSembol
+            // 
+            this.cbxSembol.AutoSize = true;
+            this.cbxSembol.Location = new System.Drawing.Point(6, 88);
+            this.cbxSembol.Name = "cbxSembol";
+            this.cbxSembol.Size = new System.Drawing.Size(61, 17);
+            this.cbxSembol.TabIndex = 3;
+            this.cbxSembol.Text = "Sembol";
+            this.cbxSembol.UseVisualStyleBackColor = true;
+            // 
+            // lblSifreAdedi
+            // 
+            this.lblSifreAdedi.AutoSize = true;
+            this.lblSifreAdedi.Location = new System.Drawing.Point(3, 138);
+            this.lblSifreAdedi.Name = "lblSifreAdedi";
+            this.lblSifreAdedi.Size = new System.Drawing.Size(61, 13);
+            this.lblSifreAdedi.TabIndex = 4;
+            this.lblSifreAdedi.Text = "Şifre Adedi:";
+            // 
+            // lblSifreUzunlugu
+            // 
+            this.lblSifreUzunlugu.AutoSize = true;
+            this.lblSifreUzunlugu.Location = new System.Drawing.Point(3, 161);
+            this.lblSifreUzunlugu.Name = "lblSifreUzunlugu";
+            this.lblSifreUzunlugu.Size = new System.Drawing.Size(76, 13);
+            this.lblSifreUzunlugu.TabIndex = 5;
+            this.lblSifreUzunlugu.Text = "Şifre Uzunluğu";
+            // 
+            // nudSifreAdedi
+            // 
+            this.nudSifreAdedi.Location = new System.Drawing.Point(85, 136);
+            this.nudSifreAdedi.Name = "nudSifreAdedi";
+            this.nudSifreAdedi.Size = new System.Drawing.Size(120, 20);
+            this.nudSifreAdedi.TabIndex = 6;
+            // 
+            // nudSifreUzunlugu
+            // 
+            this.nudSifreUzunlugu.Location = new System.Drawing.Point(85, 159);
+            this.nudSifreUzunlugu.Name = "nudSifreUzunlugu";
+            this.nudSifreUzunlugu.Size = new System.Drawing.Size(120, 20);
+            this.nudSifreUzunlugu.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(237, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(140, 231);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(6, 202);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(80, 23);
+            this.btnUret.TabIndex = 8;
+            this.btnUret.Text = "Şifre Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 265);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.gbxParametreler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbxParametreler.ResumeLayout(false);
+            this.gbxParametreler.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSifreAdedi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSifreUzunlugu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxParametreler;
+        private System.Windows.Forms.NumericUpDown nudSifreUzunlugu;
+        private System.Windows.Forms.NumericUpDown nudSifreAdedi;
+        private System.Windows.Forms.Label lblSifreUzunlugu;
+        private System.Windows.Forms.Label lblSifreAdedi;
+        private System.Windows.Forms.CheckBox cbxSembol;
+        private System.Windows.Forms.CheckBox cbxRakam;
+        private System.Windows.Forms.CheckBox cbxBuyukHarf;
+        private System.Windows.Forms.CheckBox cbxKucukHarf;
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ SifreUretme/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SifreUretme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        bool kucukHarfVarMi;
+        bool buyukHarfVarMi;
+        bool rakamVarMi;
+        bool ozelKarakterVarMi;
+        int sifreUzunlugu, sifreSayisi;
+
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            kucukHarfVarMi = cbxKucukHarf.Checked;
+            buyukHarfVarMi = cbxBuyukHarf.Checked;
+            rakamVarMi = cbxRakam.Checked;
+            ozelKarakterVarMi = cbxSembol.Checked;
+
+            sifreUzunlugu = (int)nudSifreUzunlugu.Value;
+            sifreSayisi = (int)nudSifreAdedi.Value;
+            textBox1.Text = "";
+
+            if(!kucukHarfVarMi || !buyukHarfVarMi || !rakamVarMi || !ozelKarakterVarMi)
+            {
+                MessageBox.Show("Lutfen bir parametre seçiniz.");
+            }
+            string secilenKarakterler = "";
+            if (ozelKarakterVarMi)
+            {
+                for(int i = 33; i < 48; i++)
+                {
+                    secilenKarakterler+=Convert.ToChar(i);
+                }
+                for (int i = 58; i < 65; i++)
+                {
+                    secilenKarakterler += Convert.ToChar(i);
+                }
+                for (int i = 91; i < 97; i++)
+                {
+                    secilenKarakterler += Convert.ToChar(i);
+                }
+                for (int i = 123; i < 127; i++)
+                {
+                    secilenKarakterler += Convert.ToChar(i);
+                }
+
+            }
+            if (rakamVarMi)
+            {
+                for (int i = 48; i < 58; i++)
+                {
+                    secilenKarakterler += Convert.ToChar(i);
+                }
+            }
+            if (buyukHarfVarMi)
+            {
+                for (int i = 65; i < 91; i++)
+                {
+                    secilenKarakterler += Convert.ToChar(i);
+                }
+            }
+            if (kucukHarfVarMi)
+            {
+                for (int i = 97; i < 123; i++)
+                {
+                    secilenKarakterler += Convert.ToChar(i);
+
+                }
+            }
+            for (int i = 0; i < sifreSayisi; i++)
+            {
+                string sifre = "";
+                for (int j = 0; j < sifreUzunlugu; j++)
+                {
+                    sifre += secilenKarakterler[rnd.Next(secilenKarakterler.Length)];
+                }
+                textBox1.Text += sifre;
+                textBox1.Text += "\r\n\r\n";
+            }
+
+            //if (cbxBuyukHarf.Checked && !cbxKucukHarf.Checked && !cbxRakam.Checked && !cbxSembol.Checked)
+            //{
+            //    do
+            //    {
+            //        for (int i = 0; i < nudSifreUzunlugu.Value; i++)
+            //        {
+            //            sifre += Convert.ToChar(rnd.Next(65, 91));
+            //        }
+            //        sayac++;
+            //        MessageBox.Show(sifre);
+            //    } while (nudSifreAdedi.Value >= sayac);
+            //    
+            //}
+        }
+    }
+}
